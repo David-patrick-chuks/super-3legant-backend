463ed6286cf011dc4f99481b1de446ee0c4e632d
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-25 14:27:57 +0100

    Auto-commit: file change detected

--- scrape.js
+++ scrape.js
@@ -61,7 +61,7 @@ async function downloadWithRetry(url, savePath, retries = 3) {
 
   // Define the routes you want to scrape
   const routes = [
-    'https://www.leya.law',
+    // 'https://www.leya.law',
     // 'https://www.leya.law/news',
     'https://www.leya.law/product',
     // 'https://www.leya.law/security',
