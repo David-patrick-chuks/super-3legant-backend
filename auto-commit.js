eb1642a22a135069daa80d0bad357e9b85651851
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2024-11-27 02:51:17 +0100

    Auto-commit: file change detected

--- auto-commit.js
+++ auto-commit.js
@@ -1,5 +1,5 @@
-import  { exec } from "child_process";
 
+const exec = require('child_process').exec;
 exec('git add . && git commit -m "Auto-commit: file change detected" && git push', (err, stdout, stderr) => {
     if (err) {
         console.error(`Error: ${err.message}`);
